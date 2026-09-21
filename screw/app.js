6ca7d0caa2bd9ba7bb5513e3e24fdb611ec411ea
wenzhangzhou <wenzhangzhou654@gmail.com>
2026-09-21 17:05:57 +0800

    feat: replace stub screw/app.js with full calculator

--- screw/app.js
+++ screw/app.js
@@ -1,9 +1,1 @@
-(function () {
-  'use strict';
-  var s = document.createElement('script');
-  s.src = '../app.js';
-  s.onerror = function () {
-    document.body.innerHTML = '<p style="padding:2rem;color:#b00">无法加载 ../app.js</p>';
-  };
-  document.head.appendChild(s);
-})();
+PLACEHOLDER
